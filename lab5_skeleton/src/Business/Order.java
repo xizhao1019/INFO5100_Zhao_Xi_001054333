8bb8e53ae72b7d0aea1fa85336a25f16b614087c
Xi Zhao <zhao.xi3@northeastern.edu>
2020-10-28 14:10:21 -0400

    up to date

---
+++ lab5_skeleton/src/Business/Order.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author zhaoxi
+ */
+public class Order {
+    private ArrayList<OrderItem> orderItemList;
+    private int orderNumber;
+    private static int count =0;
+    
+    public Order(){
+        count++;
+        orderNumber = count;
+        orderItemList = new ArrayList<OrderItem>();
+    }
+
+    public ArrayList<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(ArrayList<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Order.count = count;
+    }
+    
+    public OrderItem addOrderItem(Product p, int q, double price){
+        OrderItem o = new OrderItem();
+        o.setProduct(p);
+        o.setQuantity(q);
+        o.setSalesPrice(price);
+        orderItemList.add(o);
+        return o;
+    }
+    
+    public void removeOrderItem(OrderItem o){
+        orderItemList.remove(o);
+    }
+    
+}
